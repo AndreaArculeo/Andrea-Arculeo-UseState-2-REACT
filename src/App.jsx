196374c8d19e78bfbbd70d77331355847a4a0997
Andrea <andreaarcu@gmail.com>
2024-06-06 16:23:45 +0200

    useeffect-Clock

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
 import "./App.css";
 import Counter from "./Counter";
-
+import Clock from "./Clock";
 function App() {
   return (
     <>
       <div>
         <Counter />
+        <Clock />
       </div>
     </>
   );
